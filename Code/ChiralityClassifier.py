5b2ccded1eeea323fa9d82adff681b8690b49353
Ezzy Cross <145999840+EzzyCross@users.noreply.github.com>
2024-03-05 16:00:56 +0000

    added confusion matrices, ECE metric and model loading

--- Code/ChiralityClassifier.py
+++ Code/ChiralityClassifier.py
@@ -7,6 +7,10 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 import torchvision.models as models
+from torchmetrics.functional.classification import multiclass_calibration_error
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+from  matplotlib.colors import LinearSegmentedColormap
 
 class ChiralityClassifier(pl.LightningModule):
     """
@@ -39,7 +43,8 @@ class ChiralityClassifier(pl.LightningModule):
         weight_decay=0,
         step_size=5,
         gamma=0.85,
-        batch_size=16
+        batch_size=16,
+        weights=None
     ):
         super().__init__()
 
@@ -53,7 +58,12 @@ class ChiralityClassifier(pl.LightningModule):
         self.scheduler = self.schedulers[scheduler]
         self.loss_fn = nn.CrossEntropyLoss()
         self.acc = self.accuracy_metric
+        self.ece = self.ece_metric
         self.model = self.model_versions[model_version](num_classes=num_classes)
+        if weights is not None:
+            self.load_state_dict(torch.load(weights))
+        self.test_y_predicted = []
+        self.test_y_true = []
 
     def forward(self, X):
         return self.model(X)
@@ -72,26 +82,54 @@ class ChiralityClassifier(pl.LightningModule):
         preds = self(x)
         loss = self.loss_fn(preds, y)
         acc = self.acc(preds, y)
-        return loss, acc
+        ece = self.ece(preds, y)
+        return loss, acc, ece
 
     def training_step(self, batch, batch_idx):
-        loss, acc = self._step(batch)
+        loss, acc, ece = self._step(batch)
 
         self.log("train_loss", loss, on_epoch=True, prog_bar=True, logger=True)
         self.log("train_acc", acc, on_epoch=True, prog_bar=True, logger=True)
+        self.log("calibration_error", ece, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss, acc = self._step(batch)
+        loss, acc, ece = self._step(batch)
 
         self.log("val_loss", loss, on_epoch=True, prog_bar=False, logger=True)
         self.log("val_acc", acc, on_epoch=True, prog_bar=True, logger=True)
+        self.log("calibration_error", ece, on_epoch=True, prog_bar=True, logger=True)
 
     def test_step(self, batch, batch_idx):
-        loss, acc = self._step(batch)
+        loss, acc, ece = self._step(batch)
 
-        self.log("test_loss", loss, on_step=True, prog_bar=True, logger=True)
-        self.log("test_acc", acc, on_step=True, prog_bar=True, logger=True)
+        x, y_true = batch
+        y_preds = self(x)
+        self.test_y_predicted.append(y_preds)
+        self.test_y_true.append(y_true)
+
+        self.log("test_loss", loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log("test_acc", acc, on_epoch=True, prog_bar=True, logger=True)
+        self.log("calibration_error", ece, on_epoch=True, prog_bar=True, logger=True)
+        return y_true, y_preds
+
+    def on_test_epoch_end(self):
+         y_true = torch.argmax(torch.cat(self.test_y_true),dim=1)
+         y_preds = torch.argmax(torch.cat(self.test_y_predicted),dim=1)
+         cm = confusion_matrix(y_true, y_preds,normalize = 'true')
+
+         disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['S','Z','None'])
+         colours = ['#ffffcc','#a1dab4','#41b6c4','#2c7fb8','#253494']
+         custom_cmap = LinearSegmentedColormap.from_list('custom_cmap', colours, N=20)
+         disp.plot(xticks_rotation=45,cmap = custom_cmap,values_format=".3f",colorbar=False)
+         for labels in disp.text_.ravel():
+                labels.set_fontsize(26)
+         ax = disp.ax_
+         ax.set_xlabel('Predicted Labels',fontsize=12)
+         ax.set_ylabel('True Labels',fontsize=12)
+         ax.tick_params(axis='both', which='major', labelsize=12)
+         ax.tick_params(axis='both', which='minor', labelsize=10)
+         plt.savefig('confusion_test.png')
 
     def accuracy_metric(self,predicted_labels,true_labels):
         true_highest_prob = torch.argmax(true_labels, dim=1)
@@ -100,3 +138,7 @@ class ChiralityClassifier(pl.LightningModule):
         matches = torch.count_nonzero(true_highest_prob==predicted_highest_prob)
         test_accuracy = matches/true_highest_prob.shape[0]
         return test_accuracy
+    
+    def ece_metric(self,predicted_labels,true_labels):
+        metric = multiclass_calibration_error(predicted_labels, torch.argmax(true_labels, dim=1), num_classes=3, n_bins=3, norm='l1')
+        return metric
