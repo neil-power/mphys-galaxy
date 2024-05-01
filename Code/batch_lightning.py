f787ff3bd5ec0d7373731f6054fe4dd44ea9c312
neil-power <52502232+neil-power@users.noreply.github.com>
2024-05-01 18:00:04 +0100

    Added more desi predictions

--- Code/batch_lightning.py
+++ Code/batch_lightning.py
@@ -36,12 +36,12 @@ SET_CHIRALITY = None #Set to None unless you want to use galaxies from the CUT_D
 # Models:
 #resnet18,resnet34,resnet50,resnet101,resnet152,
 #ce_resnet50,lenet,g_resnet18,g_resnet50,g_lenet,g_resnet18_old
-MODEL_NAME = "g_resnet18"
+MODEL_NAME = "g_resnet50"
 CUSTOM_ID = ""
 
 USE_TENSORBOARD = True #Log to tensorboard as well as csv logger
 SAVE_MODEL = True #Save model weights to .pt file
-REPEAT_RUNS = [1,2,3,4] #Set to 1 for 1 run, or a list for specific runs
+REPEAT_RUNS = [0,1,2] #Set to 1 for 1 run, or a list for specific runs
 IMG_SIZE = 160 #This is the output size of the generated image array
 NUM_WORKERS = 11 #Number of workers in dataloader (usually set to no of CPU cores - 1)
 MAX_IMAGES = -1 #Max number of images to load (-1 for all)
